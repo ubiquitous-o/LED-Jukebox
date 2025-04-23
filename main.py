57f20861312bc7624feda29f79c304f48b3f63ee
ubiquitous-o <ooo.ubi.ooo@gmail.com>
2025-04-23 19:15:06 +0900

    Fix variable name inconsistency for track_id in main.py and add test_spotipy.py for album URL retrieval

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import config
 import spotify
 import requests
 
-track_ID = sys.argv[1]
+track_id = sys.argv[1]
 event = sys.argv[2]
 
 print("Event: " + event + "-called")
 
 if event == "playing":
-    image_url = spotify.get_album_url(track_ID)
-
+    image_url = spotify.get_album_url(track_id)
+    print(track_id)
     img = Image.open(requests.get(image_url, stream=True).raw)
     if img.size[0] != 64 or img.size[1] != 64:
         img = img.resize((64, 64), resample=Image.BICUBIC)
-    print(img)
-    img.save("/tmp/album_cover.jpg")
-    print("save image to /tmp/hogehoge/tmp/album_cover.jpg")
+    
+    # img.save("/tmp/album_cover.jpg")
+    # print("save image to /tmp/hogehoge/tmp/album_cover.jpg")
     # pixoo.draw_image(img, image_resample_mode=ImageResampleMode.SMOOTH)
     # pixoo.push()
     # print("draw album cover")
---
+++ test_spotipy.py
@@ -0,0 +1,4 @@
+import spotify
+
+track_id = "11vZhGXiQJg1ABYOSeKqB4"
+spotify.get_album_url(track_id)
